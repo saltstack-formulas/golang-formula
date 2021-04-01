f6f009b350518edc6e1feeac71d6cfbb92cc8d08
Imran Iqbal <iqbalmy@hotmail.com>
2021-04-01 12:27:39 +0100

    fix(freebsd): ensure `git` is installed and `default` tests pass

--- test/integration/default/controls/archives_spec.rb
+++ test/integration/default/controls/archives_spec.rb
@@ -1,14 +1,22 @@
 # frozen_string_literal: true
 
+dir_go, dir_go_group =
+  case system.platform[:family]
+  when 'bsd'
+    %w[/usr/local/go1.14.2.freebsd-amd64/go wheel]
+  else
+    %w[/usr/local/go1.14.2.linux-amd64/go root]
+  end
+
 control 'golang tar archive' do
   impact 1.0
   title 'should be installed'
   desc 'Ensure golang tarball archive was extracted correctly'
 
-  describe file('/usr/local/go1.14.2.linux-amd64/go') do
+  describe file(dir_go) do
     it { should be_directory }
     it { should be_owned_by 'root' }
-    it { should be_grouped_into 'root' }
+    it { should be_grouped_into dir_go_group }
     its('mode') { should cmp '0755' }
   end
 end
